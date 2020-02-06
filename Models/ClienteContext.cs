c9bec965ab7c68b85a74a274963c8166ce2e73b5
Higor Chagas <42949902+higorfchagas@users.noreply.github.com>
2020-02-06 20:50:02 -0300

    Initial commit

---
+++ Models/ClienteContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademiaWebApi.Models
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
